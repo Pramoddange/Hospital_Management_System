05e4158ce1038873c00d98872760ea079429813d
Pramod Dange <pramodsdange1@gmail.com>
2024-02-02 23:34:13 +0530

    trial

--- src/main/java/com/hospitalmanagmentsystem/Hospital/Management/System/controller/PatientController.java
+++ src/main/java/com/hospitalmanagmentsystem/Hospital/Management/System/controller/PatientController.java
@@ -1,4 +1,26 @@
 package com.hospitalmanagmentsystem.Hospital.Management.System.controller;
 
+import com.hospitalmanagmentsystem.Hospital.Management.System.models.Hospital;
+import com.hospitalmanagmentsystem.Hospital.Management.System.models.Patient;
+import com.hospitalmanagmentsystem.Hospital.Management.System.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Patient")
+
 public class PatientController {
+    @Autowired
+    PatientService patientService;
+    public String registerPatient(@RequestBody Patient obj){
+        patientService.register(obj);
+        return "Patient got register successfully";
+    }
+   // @GetMapping
+    //public Patient getByPatientId(){
+
+    //}
 }
